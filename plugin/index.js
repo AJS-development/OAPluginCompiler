032ae24f50c1250214930bd3dac2862064d9de6d
Andrew S <andrews54757@gmail.com>
2016-11-11 20:38:31 -0500

    Update index.js

--- plugin/index.js
+++ plugin/index.js
@@ -7,7 +7,7 @@ minVersion: '', // minimum version requirements (optional)
 version:  '', // version REQUIRED
 addToHelp: false, // add to help
  commands: false, // commands
-  
+ configFile: false, //config file
 // [Functions]
 init: function (data) {
   // init, Used to do stuff such as overriding things
